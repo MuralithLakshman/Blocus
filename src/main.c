dbbbb6787a6a9d67a5fcd7852fef21edd4f60842
Nathan Baudrier <nathanbaudrier@gmail.com>
2024-10-26 10:01:23 +0200

    some tests

--- src/main.c
+++ src/main.c
@@ -4,24 +4,50 @@
 #include <stdbool.h>
 #include "/usr/include/X11/keysymdef.h"
 
+/* Valeur d'une micro secondes */
 #define MICRO 1000000L 
 
-#define WIDTH 1920
-#define HEIGHT 1080
+/* Taille de la fenêtre */ 
+#define WIDTH 900 /* Largeur */ 
+#define HEIGHT 600 /* Hauteur */
+/* Valeurs approximatives que nous avons trouvé en faisant des tests */ 
+#define MAX_WIDTH 1270
+#define MAX_HEIGHT 740
+
+void drawGrid(void) {
+  int x, y = 10;
+  int i; 
+  
+  ChoisirEcran(2);
+}
+    
+  
 
 int main(void) {
   int next;
+  int screenX = (MAX_WIDTH - WIDTH) / 2;
+  int screenY = (MAX_HEIGHT - HEIGHT) / 2; 
   
   InitialiserGraphique();
-  CreerFenetre(10, 10, 900, 900);
-  EcrireTexte(10, 10, "JEUX", 12);
+  /* Créer un fenêtre toujours à peu près au milieu de l'écran */ 
+  CreerFenetre(screenX, screenY, WIDTH, HEIGHT);
 
   next = Microsecondes() + MICRO; 
   while(true) {
-    if(Touche() == XK_Escape) break;
+    /* SI la touche escape est pressée, cela arrête le programme. */ 
+    if(ToucheEnAttente() == 1) {
+      if(Touche() == XK_Escape) break;
+    }
     
     if(Microsecondes() > next) {
-      
+
+      ChoisirCouleurDessin(CouleurParNom("white"));
+      ChoisirEcran(1);
+      /*EffacerEcran(CouleurParNom("dark"));*/
+      ChoisirCouleurDessin(CouleurParNom("green"));
+      EcrireTexte(screenX + 10, screenY + 10, "ACCEUIL", 2);
+      CopierZone(1, 0, screenX, screenY, WIDTH, HEIGHT, screenX, screenY); 
+      next = Microsecondes() + MICRO;
     }
   }
 	      
