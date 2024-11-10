05a3d25a399e6f161d4705bd038b95f1993b2f4c
Nathan Baudrier <nathanbaudrier@gmail.com>
2024-11-10 22:37:34 +0100

    start game system

---
+++ src/game.h
@@ -0,0 +1,28 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "graph.h"
+#include "player.h"
+#include "grid.h"
+
+#define ONE_PLAYER 1
+#define TWO_PLAYERS 2       
+
+struct game {
+  unsigned int started; /* Partie débutée (booleen) */
+  unsigned int ended; /* Partie terminée (booleen) */
+  Grid grid;
+  Player player1;
+  Player player2;
+  int turn; /* ID du joueur à qui c'est le tour */
+};
+
+typedef struct game Game;
+
+/* Initialise une nouvelle partie */ 
+Game NewGame(int number_players, Grid grid);
+
+/* Renvoie le joueur à qui c'est le tour */ 
+Player getPlayerTurn(Game game);
+
+#endif /* GAME_H */ 
